b792a4de99015021ec97943ef2c6e3ceabfd3cf0
Practice <practice@imac-novichenko-a.local>
2018-06-11 14:28:07 +0300

    storyboard

---
+++ Cards WatchKit Extension/SecondScreenInterfaceController.m
@@ -0,0 +1,36 @@
+//
+//  SecondScreenInterfaceController.m
+//  Cards WatchKit Extension
+//
+//  Created by Practice on 11.06.2018.
+//  Copyright © 2018 Practice. All rights reserved.
+//
+
+#import "SecondScreenInterfaceController.h"
+
+@interface SecondScreenInterfaceController ()
+
+@end
+
+@implementation SecondScreenInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
